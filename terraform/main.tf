73dbd34f78690de34108c62d38203fae9b4b9090
Ayidlah29 <haldiyasneha29@gmail.com>
2025-06-19 19:14:54 +0530

    Add Lambda functions and environment variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,46 @@ resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
 output "sns_topic_arn" {
   value = aws_sns_topic.event_topic.arn
 }
+
+data "archive_file" "subscribe_lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../backend/subscribe-lambda/index.py"
+  output_path = "${path.module}/subscribe_lambda.zip"
+}
+
+resource "aws_lambda_function" "subscribe_lambda" {
+  function_name = "subscribeLambda"
+  filename      = data.archive_file.subscribe_lambda_zip.output_path
+  source_code_hash = data.archive_file.subscribe_lambda_zip.output_base64sha256
+  handler       = "index.lambda_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  environment {
+    variables = {
+      TOPIC_ARN = aws_sns_topic.event_topic.arn
+    }
+  }
+}
+
+data "archive_file" "create_event_lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/../backend/create-event-lambda/index.py"
+  output_path = "${path.module}/create_event_lambda.zip"
+}
+
+resource "aws_lambda_function" "create_event_lambda" {
+  function_name = "createEventLambda"
+  filename      = data.archive_file.create_event_lambda_zip.output_path
+  source_code_hash = data.archive_file.create_event_lambda_zip.output_base64sha256
+  handler       = "index.lambda_handler"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  environment {
+    variables = {
+      BUCKET_NAME = "event-announcement-nsj-frontend-2025"
+      TOPIC_ARN   = aws_sns_topic.event_topic.arn
+    }
+  }
+}
